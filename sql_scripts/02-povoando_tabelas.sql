dcefb70f09a15f3562e4801fb49e0abcd7c50bcf
ryofac <ryanfaustinocarvalho@gmail.com>
2024-09-04 22:11:21 -0300

    feat: adicionando funcoes e triggers iniciais relacionados a venda de itens

---
+++ sql_scripts/02-povoando_tabelas.sql
@@ -0,0 +1,18 @@
+INSERT INTO cliente (nome, cpf)
+VALUES 
+('João Silva', '12345678901'),
+('Maria Oliveira', '23456789012'),
+('Carlos Souza', '34567890123');
+
+
+INSERT INTO produto (nome, valor_unitario, qnt_em_estoque)
+VALUES 
+('Suplemento Whey Protein', 120.50, 50),
+('Camiseta Esportiva', 40.00, 100),
+('Squeeze Fitness', 25.00, 200);
+
+
+INSERT INTO funcionario (nome, telefone)
+VALUES 
+('Ana Santos', '11987654321'),
+('Lucas Lima', '11876543210');
