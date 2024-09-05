5f671265be38ad5a15be90218d98412517eacdfb
ryofac <ryanfaustinocarvalho@gmail.com>
2024-09-05 19:41:59 -0300

    refactor: formatacao

--- sql_scripts/05-criando_views.sql
+++ sql_scripts/05-criando_views.sql
@@ -1,8 +1,12 @@
-CREATE VIEW INFORMACOES_VENDAS AS SELECT 
-c.nome nome_cliente, 
-f.nome nome_funcionario, 
-v.qnt_produtos, v.valor_total,
-v.dt_venda 
-FROM cliente c 
-NATURAL LEFT JOIN VENDA V JOIN funcionario f 
-ON f.id_funcionario = v.id_funcionario;
+CREATE VIEW
+  INFORMACOES_VENDAS AS
+SELECT
+  c.nome nome_cliente,
+  f.nome nome_funcionario,
+  v.qnt_produtos,
+  v.valor_total,
+  v.dt_venda
+FROM
+  cliente c
+  NATURAL LEFT JOIN VENDA V
+  JOIN funcionario f ON f.id_funcionario = v.id_funcionario;
--- sql_scripts/06-extra.sql
+++
@@ -1,1 +0,0 @@
-SELECT 'OLÁ AMIGO!';
