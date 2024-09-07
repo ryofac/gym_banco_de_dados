2010ba0891618174117b54180de580033a67f150
ryofac <ryanfaustinocarvalho@gmail.com>
2024-09-07 15:56:45 -0300

    feat: adiciona dados de insercao para sistema de academia

--- sql_scripts/02-povoando_tabelas.sql
+++ sql_scripts/02-povoando_tabelas.sql
@@ -1,3 +1,4 @@
+-- VENDAS
 INSERT INTO
   cliente (nome, cpf)
 VALUES
@@ -17,3 +18,59 @@ INSERT INTO
 VALUES
   ('Ana Santos', '11987654321'),
   ('Lucas Lima', '11876543210');
+
+-- ACADEMIA
+INSERT INTO
+  instrutor (nome, cpf, url_certificado)
+VALUES
+  (
+    'João Silva',
+    '12345678901',
+    'http://certificados.exemplo.com/joao_silva.pdf'
+  ),
+  (
+    'Maria Oliveira',
+    '98765432100',
+    'http://certificados.exemplo.com/maria_oliveira.pdf'
+  ),
+  (
+    'Carlos Souza',
+    '19283746500',
+    'http://certificados.exemplo.com/carlos_souza.pdf'
+  );
+
+INSERT INTO
+  equipamento (nome, descricao)
+VALUES
+  ('Esteira', 'Equipamento para corrida e caminhada'),
+  (
+    'Bicicleta Ergométrica',
+    'Equipamento para exercícios de ciclismo'
+  ),
+  ('Halter', 'Equipamento para levantamento de peso');
+
+INSERT INTO
+  tipo_exercicio (regiao_trabalhada)
+VALUES
+  ('Cardiovascular'),
+  ('Musculação'),
+  ('Flexibilidade');
+
+INSERT INTO
+  exercicio (id_eq, id_tipo_exercicio, nome)
+VALUES
+  (1, 1, 'Corrida na Esteira'),
+  (2, 1, 'Pedalada na Bicicleta'),
+  (3, 2, 'Supino com Halter'),
+  (3, 2, 'Rosca Direta com Halter'),
+  (1, 3, 'Alongamento na Esteira');
+
+INSERT INTO
+  equipamento (nome, descricao)
+VALUES
+  ('Esteira', 'Equipamento para corrida e caminhada'),
+  (
+    'Bicicleta Ergométrica',
+    'Equipamento para exercícios de ciclismo'
+  ),
+  ('Halter', 'Equipamento para levantamento de peso');
