e94732f3919bd323687dcef86f4a3374732f517a
LuisF Patrocinio <patrocinioluisf@gmail.com>
2024-09-14 17:31:15 -0300

    feat: function to register a new instructor

--- sql_scripts/07-funcoes-de-cadastro.sql
+++ sql_scripts/07-funcoes-de-cadastro.sql
@@ -21,4 +21,27 @@ END;
 $$
 LANGUAGE plpgsql;
 
+-- Função para cadastrar um novo instrutor
+CREATE OR REPLACE FUNCTION CADASTRAR_INSTRUTOR(
+    NOME VARCHAR, 
+    CPF VARCHAR(11),
+    URL_CERTIFICADO VARCHAR(255)
+)
+RETURNS VOID AS $$
+BEGIN
+    -- Validar CPF
+    IF LENGTH(CPF) != 11 THEN
+        RAISE EXCEPTION 'CPF % inválido!', CPF;
+    END IF;
+
+    -- Verificar se o instrutor já existe
+    IF EXISTS(SELECT * FROM INSTRUTOR WHERE CPF = CPF) THEN
+        RAISE EXCEPTION 'Instrutor com CPF % já cadastrado!', CPF;
+    END IF;
+
+    -- Inserir instrutor na tabela
+    INSERT INTO INSTRUTOR VALUES(DEFAULT, NOME, CPF, URL_CERTIFICADO);
+END;
+$$
+
 
