3e453321ac885cb8b136f56f0b2f130b13cfe38c
LuisF Patrocinio <patrocinioluisf@gmail.com>
2024-09-18 16:23:46 -0300

    Casos de teste de compras.

---
+++ sql_scripts/10-testes.sql
@@ -0,0 +1,9 @@
+-- Testar funcionamento do banco.
+
+-- Criar novo cliente
+SELECT CADASTRAR_CLIENTE('Luis F. Patrocinio', '12345678901');
+
+-- Criar novo instrutor
+SELECT CADASTRAR_INSTRUTOR('Prof. Rodrigo Góes', '12345678902', 'http://www.google.com');
+
+--
