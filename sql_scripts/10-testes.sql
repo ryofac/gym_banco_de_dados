e92652d8a4c595e0611ab2de19e147b56952c86e
LuisF Patrocinio <patrocinioluisf@gmail.com>
2024-09-21 08:35:27 -0300

    Funções de relatório e função para obter último id de compra pendente.

--- sql_scripts/10-testes.sql
+++
@@ -1,9 +0,0 @@
--- Testar funcionamento do banco.
-
--- Criar novo cliente
-SELECT CADASTRAR_CLIENTE('Luis F. Patrocinio', '12345678901');
-
--- Criar novo instrutor
-SELECT CADASTRAR_INSTRUTOR('Prof. Rodrigo Góes', '12345678902', 'http://www.google.com');
-
---
